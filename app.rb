1cc6c982d4ed6b9b4e8aca1a6afaec7492824095
Idude101 <loneayman101@gmail.com>
2025-01-28 22:09:24 +0000

    rps

--- app.rb
+++ app.rb
@@ -7,3 +7,45 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else @comp_move == "scissors"
+      @outcome = "won"
+    end
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    else @comp_move == "scissors"
+      @outcome = "lost"
+    end
+  erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    else @comp_move == "scissors"
+      @outcome = "tied"
+    end
+  erb(:scissors)
+end
